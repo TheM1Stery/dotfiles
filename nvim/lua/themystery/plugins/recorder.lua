efe3c458042c80d9287dbc49d61e52e5d6deb433
Seymur Bagirov <megadestroyer230@gmail.com>
2024-07-05 18:30:23 +0400

    fix: change keymaps of nvim-recorder

--- nvim/lua/themystery/plugins/recorder.lua
+++ nvim/lua/themystery/plugins/recorder.lua
@@ -1,5 +1,16 @@
 return {
     "chrisgrieser/nvim-recorder",
     dependencies = "rcarriga/nvim-notify", -- optional
-    opts = {},                          -- required even with default settings, since it calls `setup()`
+    opts = {
+        mapping = {
+            startStopRecording = "<leader>q",
+            playMacro = "Q",
+            switchSlot = "<C-q>",
+            editMacro = "cq",
+            deleteAllMacros = "dq",
+            yankMacro = "yq",
+            -- ⚠️ this should be a string you don't use in insert mode during a macro
+            addBreakPoint = "##",
+        }
+    },
 }
