b57db03674b8c893fcf333d712f9391e475bd80b
Seymur Bagirov <seymur.baghirov@proton.me>
2025-02-16 17:28:25 +0400

    chore: remove unused snacks.nvim plugins

--- nvim/lua/themystery/plugins/snacks.lua
+++ nvim/lua/themystery/plugins/snacks.lua
@@ -7,9 +7,7 @@ return {
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
         bigfile = { enabled = true },
-        notifier = { enabled = false },
         quickfile = { enabled = true },
-        statuscolumn = { enabled = false },
         words = { enabled = true },
         dashboard = {
             enabled = true,
