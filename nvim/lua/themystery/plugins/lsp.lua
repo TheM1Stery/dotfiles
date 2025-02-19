63965068e6f7c52ab5e9aaf845d8f1cb689ad385
Seymur Bagirov <seymur.baghirov@proton.me>
2025-02-19 17:36:27 +0400

    chore: update crates and fix crates.nvim

--- nvim/lua/themystery/plugins/lsp.lua
+++ nvim/lua/themystery/plugins/lsp.lua
@@ -7,7 +7,7 @@ return {
     },
     {
         'saecki/crates.nvim',
-        tag = 'stable',
+        event = { "BufRead Cargo.toml" },
         config = function()
             require('crates').setup({
                 lsp = {
