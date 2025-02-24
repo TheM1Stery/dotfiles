cbf70bfd51d4f3a3afe4270267c9cb02432a9f64
Seymur Bagirov <seymur.baghirov@proton.me>
2025-02-24 19:45:27 +0400

    tweak blink.cmp

--- nvim/lua/themystery/plugins/completion.lua
+++ nvim/lua/themystery/plugins/completion.lua
@@ -11,7 +11,10 @@ return {
         -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
         -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
         -- See the full "keymap" documentation for information on defining your own keymap.
-        keymap = { preset = 'super-tab' },
+        keymap = { preset = 'super-tab', ['<CR>'] = { 'accept', 'fallback' } },
+        cmdline = {
+            keymap = { preset = 'super-tab' }
+        },
         completion = {
             menu = { border = 'single' },
             documentation = { window = { border = 'single' } },
