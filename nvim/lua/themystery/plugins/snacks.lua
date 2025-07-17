2d48389b8192a2f998c63b82e71cf2bb6f36cf2f
Seymur Bagirov <seymur.baghirov@proton.me>
2025-07-17 22:52:50 +0400

    use snacks image instead of image.nvim

--- nvim/lua/themystery/plugins/snacks.lua
+++ nvim/lua/themystery/plugins/snacks.lua
@@ -9,6 +9,7 @@ return {
         bigfile = { enabled = true },
         quickfile = { enabled = true },
         words = { enabled = true },
+        image = { enabled = true },
         dashboard = {
             enabled = true,
             sections = {
