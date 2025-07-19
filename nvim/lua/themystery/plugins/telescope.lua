4982e8997a8267d43c6911e1ce2a92f3b5be617d
Seymur Bagirov <seymur.baghirov@proton.me>
2025-07-20 03:59:18 +0400

    feat(neovim): add telescope-fzf-native extension to telescope I love fzf

--- nvim/lua/themystery/plugins/telescope.lua
+++ nvim/lua/themystery/plugins/telescope.lua
@@ -1,80 +1,86 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    branch = "master",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope-ui-select.nvim",
-        "olimorris/persisted.nvim"
-    },
-    config = function()
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
-        vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-        vim.keymap.set('n', '<leader>pd', builtin.diagnostics, {})
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    {
+
+        "nvim-telescope/telescope.nvim",
+        branch = "master",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope-ui-select.nvim",
+            "olimorris/persisted.nvim",
+            "nvim-telescope/telescope-fzf-native.nvim"
+        },
+        config = function()
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+            vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+            vim.keymap.set('n', '<leader>ps', function()
+                builtin.grep_string({ search = vim.fn.input("Grep > ") })
+            end)
+            vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
+            vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+            vim.keymap.set('n', '<leader>pd', builtin.diagnostics, {})
 
 
-        local actions = require('telescope.actions')
+            local actions = require('telescope.actions')
 
-        require('telescope').setup {
-            extensions = {
-                ["ui-select"] = {
-                    require("telescope.themes").get_cursor {}
-                },
-            },
-            defaults = {
-                mappings = {
-                    i = {
-                        ["<C-s>"] = actions.select_vertical,
-                    },
-                    n = {
-                        ["<C-s>"] = actions.select_vertical,
+            require('telescope').setup {
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_cursor {}
                     },
                 },
-            },
-            pickers = {
-                find_files = {
-                    theme = "dropdown"
-                },
-                git_files = {
-                    theme = "dropdown"
-                },
-                grep_string = {
-                    theme = "dropdown"
-                },
-                buffers = {
-                    theme = "dropdown",
+                defaults = {
                     mappings = {
-                        n = {
-                            ['<C-d>'] = actions.delete_buffer
-                        },
                         i = {
-                            ['<C-d>'] = actions.delete_buffer
-                        }
-                    }
+                            ["<C-s>"] = actions.select_vertical,
+                        },
+                        n = {
+                            ["<C-s>"] = actions.select_vertical,
+                        },
+                    },
                 },
+                pickers = {
+                    find_files = {
+                        theme = "dropdown"
+                    },
+                    git_files = {
+                        theme = "dropdown"
+                    },
+                    grep_string = {
+                        theme = "dropdown"
+                    },
+                    buffers = {
+                        theme = "dropdown",
+                        mappings = {
+                            n = {
+                                ['<C-d>'] = actions.delete_buffer
+                            },
+                            i = {
+                                ['<C-d>'] = actions.delete_buffer
+                            }
+                        }
+                    },
+                }
             }
-        }
-        require("telescope").load_extension("ui-select")
-        require("telescope").load_extension("persisted")
+            require("telescope").load_extension("ui-select")
+            require("telescope").load_extension("persisted")
+            require("telescope").load_extension("fzf")
 
 
-        vim.keymap.set("n", '<leader>ops', ":Telescope persisted<CR>")
+            vim.keymap.set("n", '<leader>ops', ":Telescope persisted<CR>")
 
-        -- persisted, remove buffers after switching sessions
-        vim.api.nvim_create_autocmd("User", {
-            pattern = "PersistedTelescopeLoadPre",
-            callback = function(session)
-                -- Save the currently loaded session using the global variable
-                require("persisted").save({ session = vim.g.persisted_loaded_session })
+            -- persisted, remove buffers after switching sessions
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "PersistedTelescopeLoadPre",
+                callback = function(session)
+                    -- Save the currently loaded session using the global variable
+                    require("persisted").save({ session = vim.g.persisted_loaded_session })
 
-                -- Delete all of the open buffers
-                vim.api.nvim_input("<ESC>:%bd!<CR>")
-            end,
-        })
-    end
+                    -- Delete all of the open buffers
+                    vim.api.nvim_input("<ESC>:%bd!<CR>")
+                end,
+            })
+        end
+    }
 }
