15360cb941922af9e7ee3b7dcb0362b16ebc7c4b
Seymur Bagirov <seymur.baghirov@proton.me>
2025-11-12 17:14:03 +0400

    fix: remove autoformat from the go plugin it breaks sometimes

--- nvim/lua/themystery/plugins/lsp.lua
+++ nvim/lua/themystery/plugins/lsp.lua
@@ -51,14 +51,14 @@ return {
         },
         config = function(lp, opts)
             require("go").setup(opts)
-            local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                pattern = "*.go",
-                callback = function()
-                    require('go.format').goimports()
-                end,
-                group = format_sync_grp,
-            })
+            -- local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+            -- vim.api.nvim_create_autocmd("BufWritePre", {
+            --     pattern = "*.go",
+            --     callback = function()
+            --         require('go.format').goimports()
+            --     end,
+            --     group = format_sync_grp,
+            -- })
         end,
         event = { "CmdlineEnter" },
         ft = { "go", 'gomod' },
@@ -76,14 +76,14 @@ return {
         event = "LspAttach",
         opts = {}, -- required, even if empty
     },
-    {
-        'oribarilan/lensline.nvim',
-        tag = '2.0.0', -- or: branch = 'release/2.x' for latest non-breaking updates
-        event = 'LspAttach',
-        config = function()
-            require("lensline").setup()
-        end,
-    },
+    -- {
+    --     'oribarilan/lensline.nvim',
+    --     tag = '2.0.0', -- or: branch = 'release/2.x' for latest non-breaking updates
+    --     event = 'LspAttach',
+    --     config = function()
+    --         require("lensline").setup()
+    --     end,
+    -- },
     {
         'neovim/nvim-lspconfig',
         dependencies = {
