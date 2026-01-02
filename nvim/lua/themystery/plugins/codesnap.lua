aee41a519927ca5c64de86a2d83581f94a19de18
Seymur Bagirov <seymur.baghirov@proton.me>
2026-01-02 14:01:21 +0400

    chore(nvim): update plugins

--- nvim/lua/themystery/plugins/codesnap.lua
+++ nvim/lua/themystery/plugins/codesnap.lua
@@ -1,15 +1,8 @@
 return {
     "mistricky/codesnap.nvim",
-    config = function()
-        require("codesnap").setup({
-            mac_window_bar = false,
-            watermark = "TheMystery",
-            has_breadcrumbs = true,
-            bg_theme = "grape",
-            has_line_number = true
-        })
-
-        vim.keymap.set("v", "<leader>cc", "<cmd>CodeSnap<cr>")
-    end,
-    build = "make"
+    keys = {
+        { "<leader>cc", "<cmd>CodeSnap<cr>", desc = "take a code snap" }
+    },
+    opts = {}
+    -- build = "make"
 }
